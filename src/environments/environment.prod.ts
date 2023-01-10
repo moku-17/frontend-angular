863381ca3eec8bafad64a78d01abfe144f0212b6
Mónica <mnk_17@hotmail.com>
2023-01-10 13:35:51 -0300

    backend en render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl:"https://backend-springboot-moku-17.herokuapp.com"
+  apiBaseUrl:"https://backend-springboot-moku-17.onrender.com"
 };
